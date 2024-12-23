7b0a65138f1a49d97beefadeca73feb7de782f35
Aniso13 <mohamedanis098765@gmail.com>
2024-12-23 14:04:59 +0200

    Graph Update

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,18 +1,15 @@
 package com.example;
 
+import Graph.Graph;
 import HashTable.HashTable;
 
 
 public class Main {
     public static void main(String[] args) {
-        HashTable myHashTable = new HashTable();
-
-        myHashTable.set("nails", 100);
-        myHashTable.set("tile", 50);
-        myHashTable.set("lumber", 80);
-        myHashTable.set("bolts", 200);
-        myHashTable.set("screws", 140);
-
-        myHashTable.printTable();
+        Graph graph = new Graph();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addEdge("A", "B");
+        graph.PrintGraph();
     }
 }
